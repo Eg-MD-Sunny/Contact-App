090a35431da59475c4d620b9e10b3e230c80e2d8
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-03-28 00:06:40 +0600

    Modify

--- js/app.js
+++ js/app.js
@@ -19,3 +19,58 @@ lActive.onclick = function() {
     s_active_el.style.zIndex = "1";
     s_active_el.classList = "animate__animated animate__fadeInDown active-box l-active";
 }
+
+
+/*Start SignUp*/
+let f_name = document.querySelector('#f-name');
+let l_name = document.querySelector('#l-name');
+let s_username = document.querySelector('#s-username');
+let s_password = document.querySelector('#s-password');
+let signup_btn = document.querySelector('.signup-btn');
+let s_notic = document.getElementById('s-notic');
+
+signup_btn.onclick = function(e) {
+    e.preventDefault();
+
+    if(f_name.value != "" || l_name.value != "" || s_username.value != "" || s_password.value != ""){
+        if(localStorage.getItem(s_username.value) == null){
+            let data = {
+                f_name : f_name.value,
+                l_name : l_name.value,
+                s_username : s_username.value,
+                s_password : s_password.value
+            }
+            // console.log(data);
+            let s_string = JSON.stringify(data);
+            console.log(s_string);
+            localStorage.setItem(s_username.value,s_string);
+            s_notic.innerHTML = "SignUp Success !";
+
+            s_notic.style.color = "green";
+            s_notic.style.marginTop = "12px";
+            setTimeout(()=>{
+                s_notic.innerHTML = "";
+            },3000)
+               f_name.value = "";
+               l_name.value = "";
+               s_username.value = "";
+               s_password.value = "";
+
+        }else{
+            s_notic.innerHTML = "Username Already Exist";
+            s_notic.style.color = "red";
+            s_notic.style.marginTop = "12px";
+            setTimeout(()=>{
+                s_notic.innerHTML = "";
+            },3000)
+        }
+    }else{
+        s_notic.innerHTML = "Please fill all fields!";
+        s_notic.style.color = "red";
+        s_notic.style.marginTop = "12px";
+        setTimeout(()=>{
+            s_notic.innerHTML = "";
+        },3000)
+    }
+    
+}
